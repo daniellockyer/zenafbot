6be53f0b175b684c8de3e0e8712b2fdc9e743f88
Daniel Lockyer <thisisdaniellockyer@gmail.com>
2018-03-24 12:24:36 +0000

    Fix string continuation bug

--- bot/bot.py
+++ bot/bot.py
@@ -126,8 +126,8 @@ def schedulereminders(bot, update):
     if has_pm_bot is True:
         bot.send_message(chat_id=update.message.from_user.id, text="Okay {}, I've scheduled those reminders for you! 🕑".format(username))
     else:
-        bot.send_message(chat_id=update.message.chat.id, text="Okay {}, I've scheduled those reminders for you! 🕑 "/
-                            "If you haven't already, please send me a PM at @zenafbot so that I can PM your reminders to you!")
+        bot.send_message(chat_id=update.message.chat.id, text="Okay {}, I've scheduled those reminders for you! 🕑 "\
+                        "If you haven't already, please send me a PM at @zenafbot so that I can PM your reminders to you!")
 
 
 def executereminders(bot, job):
